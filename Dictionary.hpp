f4c7d89c0152799793f2ce5b4526c00df6dbd20c
Ran <ranhoudine@gmail.com>
2022-06-15 21:52:02 +0300

    2

---
+++ Dictionary.hpp
@@ -0,0 +1,36 @@
+#include "HashMap.hpp"
+
+#ifndef _DICTIONARY_HPP_
+#define _DICTIONARY_HPP_
+using std::string;
+class InvalidKey: std::invalid_argument
+{
+ public:
+  explicit InvalidKey(const string& what): std::invalid_argument(what) {}
+};
+class Dictionary : public HashMap<string, string>
+{
+ public:
+  bool erase (const string &key);
+  void update (HashMap::ConstIterator& start, const
+  HashMap::ConstIterator& end );
+
+};
+bool Dictionary::erase (const string &key)
+{
+  if (!HashMap::erase(key))
+  {
+    return true;
+  }
+  throw InvalidKey(KEY_NOT_FOUND_MSG);
+}
+void
+Dictionary::update (HashMap::ConstIterator &start, const HashMap::ConstIterator &end)
+{
+  while (start != end && start.operator->() != nullptr)
+  {
+    insert (start->first, start->second);
+    ++start;
+  }
+}
+#endif //_DICTIONARY_HPP_
--- HashMap.hpp
+++ HashMap.hpp
@@ -2,102 +2,520 @@
 #ifndef _HASHMAP_HPP_
 #define _HASHMAP_HPP_
 
-#define MIN_CAPACITY 16
+#define MIN_CAPACITY 1
+#define DEFAULT_CAPACITY 16
 #define LOWER_LOAD_FACTOR 0.25
 #define UPPER_LOAD_FACTOR 0.75
 #define BAD_VECTORS_LENGTH_MSG "Constructor received vectors of different lengths"
+#define KEY_NOT_FOUND_MSG "Key not found"
 
 #include <vector>
 #include <stdexcept>
+#include "HashMap.h"
 using std::vector;
 using std::pair;
-typedef int ValueT; // just for working with the ints
-typedef int KeyT; // just for working with the ints
+
+template<class KeyT, class ValueT>
 class HashMap // at first working with ints
 {
  public:
   /*** Constructors and Destructors ***/
 
   HashMap ();
-  HashMap (vector<int> keys, vector<int> values);
-  HashMap (const HashMap &map);
+  HashMap (vector<KeyT> keys, vector<ValueT> values);
+  HashMap (const HashMap<KeyT, ValueT> &map);
   ~HashMap ();
 
   /*** Methods ***/
 
-  int size () const;
-  int capacity () const;
-  bool empty () const;
-  bool insert (KeyT key, ValueT value);
-  bool contains_key (KeyT key) const;
-  ValueT at (KeyT key);
-  bool erase (KeyT key);
+  int size () const
+  {
+    return _size;
+  }
+  int capacity () const
+  {
+    return _capacity;
+  }
+  bool empty () const
+  {
+    return (_size == 0);
+  }
+  bool insert (const KeyT &key, const ValueT &value);
+  bool contains_key (const KeyT &key) const;
+  ValueT &at (const KeyT &key);
+  const ValueT &at (const KeyT &key) const;
+  virtual bool erase (const KeyT &key);
   double get_load_factor () const;
-  int bucket_size (KeyT key) const;
-  int bucket_index (KeyT key) const;
+  int bucket_size (const KeyT &key) const;
+  int bucket_index (const KeyT &key) const;
   void clear ();
 
   /*** Operators ***/
-  HashMap &operator= (const HashMap &other);
+  HashMap<KeyT, ValueT> &operator= (const HashMap<KeyT, ValueT> &other);
   ValueT &operator[] (const KeyT &key);
-  bool operator== (const HashMap &other);
-  bool operator!= (const HashMap &other);
+  bool operator== (const HashMap<KeyT, ValueT> &other);
+  bool operator!= (const HashMap<KeyT, ValueT> &other);
 
+  /*** Forward declarations ***/
+  class ConstIterator;
+
+  /*** typedefs ***/
+  typedef ConstIterator const_iterator;
+
+  /*** Begins and Ends ***/
+
+  const_iterator begin () const
+  {
+    if (empty ())
+    { return end (); }
+    int bucket_index = 0;
+    while (buckets[bucket_index].empty ())
+    {
+      bucket_index++;
+    }
+    return ConstIterator (&(buckets[bucket_index][0]), this, 0, bucket_index);
+  }
+  const_iterator end () const
+  {
+    return HashMap<KeyT, ValueT>::const_iterator (nullptr, this, 0, 0);
+  }
+  const_iterator cbegin () const
+  {
+    if (empty ())
+    { return end (); }
+    int bucket_index = 0;
+    while (buckets[bucket_index].empty ())
+    {
+      bucket_index++;
+    }
+    return ConstIterator (&(buckets[bucket_index][0]), this, 0, bucket_index);
+  }
+  const_iterator cend () const
+  {
+    return HashMap<KeyT, ValueT>::const_iterator (nullptr, this, 0, 0);
+  }
  private:
   unsigned int _capacity;
   unsigned int _size;
   double upper_load_factor = UPPER_LOAD_FACTOR;
   double lower_load_factor = LOWER_LOAD_FACTOR;
   vector<pair<KeyT, ValueT>> *buckets;
-  std::hash<ValueT> value_hash;
-
+  std::hash<KeyT> value_hash;
+  bool contained_in (const HashMap<KeyT, ValueT> &other) const;
   /*** private methods ***/
 
   void create_buckets ();
-  unsigned int hash (KeyT key);
+  unsigned int hash (KeyT key) const;
   void re_hash ();
-  int first_power_of_2_larger_than (unsigned long size);
+  int first_power_of_2_larger_than (int size)
+  {
+    int num = 1;
+    while (num < size)
+    {
+      num *= 2;
+    }
+    return num;
+  }
+  void adjust_capacity_to_balance_load_factor ();
+  bool load_factor_okay ();
+};
+template<class KeyT, class ValueT>
+class HashMap<KeyT, ValueT>::ConstIterator
+{
+ public:
+  /*** Constructors ***/
+  ConstIterator (const pair<KeyT, ValueT> *_p, const HashMap<KeyT, ValueT> *map, int i_w,
+                 int b_i) : p (_p), associated_map (map), index_within_bucket
+      (i_w), bucket_index (b_i)
+  {}
+
+  /*** Typedefs***/
+
+  typedef const pair<KeyT, ValueT> value_type;
+  typedef const pair<KeyT, ValueT> &reference;
+  typedef const pair<KeyT, ValueT> *pointer;
+  typedef std::ptrdiff_t difference_type;
+  typedef std::forward_iterator_tag iterator_category;
+
+  /*** Operators ***/
+
+  ConstIterator &operator++ ()
+  {
+    if ((int) index_within_bucket + 1 < associated_map->bucket_size (p->first))
+    {
+      index_within_bucket++;
+      p = &(associated_map->buckets[bucket_index][index_within_bucket]);
+      return *this;
+    }
+    index_within_bucket = 0;
+    bucket_index++;
+    while (bucket_index < associated_map->_capacity &&
+           associated_map->buckets[bucket_index].empty ())
+    {
+      bucket_index++;
+    }
+    if (bucket_index < associated_map->_capacity)
+    {
+      p = &(associated_map->buckets[bucket_index][index_within_bucket]);
+      return *this;
+    }
+    this->p = nullptr;
+    this->bucket_index = 0;
+    this->index_within_bucket = 0;
+    return *this;
+  }
+  ConstIterator operator++ (int)
+  {
+    HashMap<KeyT, ValueT>::ConstIterator it (p, associated_map, index_within_bucket,
+                                             bucket_index);
+    if (index_within_bucket + 1 < associated_map->bucket_size (p->first)) //
+      // if it's not the last pair in the bucket
+    {
+      index_within_bucket++;
+      p = &(associated_map->buckets[bucket_index][index_within_bucket]);
+      return it;
+    }
+    index_within_bucket = 0;
+    bucket_index++;
+    while (bucket_index < associated_map->_capacity &&
+           associated_map->buckets[bucket_index].empty ())
+    {
+      bucket_index++;
+    }
+    if (bucket_index + 1 <= associated_map->_capacity)
+    {
+      p = &(associated_map->buckets[bucket_index][index_within_bucket]);
+      return it;
+    }
+    this->p = nullptr;
+    this->bucket_index = 0;
+    this->index_within_bucket = 0;
+    return it;
+  }
+  bool operator== (const HashMap<KeyT, ValueT>::ConstIterator &other)
+  {
+    return p == other.p && associated_map == other.associated_map;
+  }
+  bool operator!= (const HashMap<KeyT, ValueT>::ConstIterator &other)
+  {
+    return !(operator== (other));
+  }
+  value_type operator* ()
+  {
+    return *p;
+  }
+  pointer operator-> ()
+  {
+    return p;
+  }
+
+ private:
+  const pair<KeyT, ValueT> *p;
+  const HashMap<KeyT, ValueT> *associated_map;
+  unsigned int index_within_bucket;
+  unsigned int bucket_index;
+
 };
 
-HashMap::HashMap () : HashMap( vector<KeyT>(), vector<ValueT>())
+template<class KeyT, class ValueT>
+HashMap<KeyT, ValueT>::HashMap () : HashMap (vector<KeyT> (), vector<ValueT>
+    ())
 {}
 
 /*** private methods ***/
-void HashMap::create_buckets ()
+template<class KeyT, class ValueT>
+void HashMap<KeyT, ValueT>::create_buckets ()
 {
   buckets = new vector<pair<KeyT, ValueT>>[_capacity];
-  for (int i = 0; i < _capacity; ++i)
+  for (unsigned int i = 0; i < _capacity; ++i)
   {
     buckets[i] = vector<pair<KeyT, ValueT>> ();
   }
 }
 
-unsigned int HashMap::hash (KeyT key)
+template<class KeyT, class ValueT>
+unsigned int HashMap<KeyT, ValueT>::hash (KeyT key) const
 {
   unsigned int hash_value = value_hash (key);
   return hash_value & (_capacity - 1);
 }
 
-HashMap::HashMap (vector<KeyT> keys, vector<ValueT> values)
+template<class KeyT, class ValueT>
+HashMap<KeyT, ValueT>::HashMap (vector<KeyT> keys, vector<ValueT> values)
 {
   if (keys.size () != values.size ())
   {
     throw std::length_error (BAD_VECTORS_LENGTH_MSG);
   }
-  _capacity = (MIN_CAPACITY > keys.size()) ? MIN_CAPACITY :
-      first_power_of_2_larger_than(keys.size());
-  _size = keys.size();
-  create_buckets();
-  for (int i = 0; i < _size; ++i)
+  _capacity = (DEFAULT_CAPACITY > keys.size ()) ? DEFAULT_CAPACITY :
+              first_power_of_2_larger_than (keys.size ());
+  _size = 0;
+  create_buckets ();
+  for (size_t i = 0; i < keys.size (); ++i)
   {
-    buckets[hash (keys[i])].push_back ({keys[i], values[i]});
+    insert (keys[i], values[i]);
   }
 }
 
-void HashMap::re_hash ()
+template<class KeyT, class ValueT>
+void HashMap<KeyT, ValueT>::re_hash ()
 {
-
+  vector<pair<KeyT, ValueT>> *original_buckets = buckets;
+  unsigned int original_capacity = _capacity;
+  adjust_capacity_to_balance_load_factor ();
+  create_buckets ();
+  _size = 0;
+  for (unsigned int i = 0; i < original_capacity; i++)
+  {
+    vector<pair<KeyT, ValueT>> v = original_buckets[i];
+    for (int j = 0; j < v.size (); ++j)
+    {
+      unsigned int hash_value = hash (v[j].first);
+      buckets[hash_value].push_back ({v[j].first, v[j].second});
+      _size++;
+    }
+  }
+  delete[] original_buckets;
 }
 
+template<class KeyT, class ValueT>
+HashMap<KeyT, ValueT>::~HashMap ()
+{
+  delete[] buckets;
+}
+
+template<class KeyT, class ValueT>
+ValueT &HashMap<KeyT, ValueT>::at (const KeyT &key)
+{
+  unsigned int hash_value = hash (key);
+  for (size_t i = 0; i < buckets[hash_value].size (); ++i)
+  {
+    if (buckets[hash_value].at (i).first == key)
+    {
+      return buckets[hash_value].at (i).second;
+    }
+  }
+  throw std::runtime_error (KEY_NOT_FOUND_MSG);
+}
+
+template<class KeyT, class ValueT>
+HashMap<KeyT, ValueT> &
+HashMap<KeyT, ValueT>::operator= (const HashMap<KeyT, ValueT> &other)
+{
+  if (this == &other)
+  {
+    return *this;
+  }
+  delete[] buckets;
+  _capacity = other._capacity;
+  _size = other._size;
+  create_buckets ();
+  for (unsigned int i = 0; i < _capacity; ++i)
+  {
+    buckets[i] = other.buckets[i];
+  }
+  return *this;
+}
+
+template<class KeyT, class ValueT>
+HashMap<KeyT, ValueT>::HashMap (const HashMap<KeyT, ValueT> &map)
+{
+  _size = map._size;
+  _capacity = map._capacity;
+  create_buckets ();
+  for (int i = 0; i < _capacity; ++i)
+  {
+    buckets[i] = map.buckets[i];
+  }
+}
+
+template<class KeyT, class ValueT>
+bool HashMap<KeyT, ValueT>::insert (const KeyT &key, const ValueT &value)
+{
+  if (!contains_key (key))
+  {
+    unsigned int hash_value = hash (key);
+    buckets[hash_value].push_back ({key, value});
+    _size++;
+    if (!load_factor_okay ())
+    {
+      re_hash ();
+    }
+    return true;
+  }
+  return false;
+}
+
+template<class KeyT, class ValueT>
+bool HashMap<KeyT, ValueT>::contains_key (const KeyT &key) const
+{
+  unsigned int hash_value = hash (key);
+  size_t size = buckets[hash_value].size ();
+  for (size_t i = 0; i < size; ++i)
+  {
+    if (buckets[hash_value].at (i).first == key)
+    {
+      return true;
+    }
+  }
+  return false;
+}
+
+template<class KeyT, class ValueT>
+double HashMap<KeyT, ValueT>::get_load_factor () const
+{
+  return (double) _size / (double) _capacity;
+}
+
+template<class KeyT, class ValueT>
+int HashMap<KeyT, ValueT>::bucket_size (const KeyT &key) const
+{
+  if (!contains_key (key))
+  {
+    throw std::runtime_error (KEY_NOT_FOUND_MSG);
+  }
+  unsigned int hash_value = hash (key);
+  return (int) buckets[hash_value].size ();
+}
+
+template<class KeyT, class ValueT>
+bool HashMap<KeyT, ValueT>::erase (const KeyT &key)
+{
+  if (contains_key (key))
+  {
+    unsigned int hash_value = hash (key);
+    for (auto it = buckets[hash_value].begin (); it < buckets[hash_value].end
+        (); it++)
+    {
+      if (it->first == key)
+      {
+        buckets[hash_value].erase (it);
+        _size--;
+        if (!load_factor_okay ())
+        {
+          re_hash ();
+        }
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+template<class KeyT, class ValueT>
+int HashMap<KeyT, ValueT>::bucket_index (const KeyT &key) const
+{
+  if (contains_key (key))
+  {
+    unsigned int hash_value = hash (key);
+    return (int) hash_value;
+  }
+  throw std::runtime_error (KEY_NOT_FOUND_MSG);
+}
+
+template<class KeyT, class ValueT>
+void HashMap<KeyT, ValueT>::clear ()
+{
+  for (int i = 0; i < _capacity; ++i)
+  {
+    _size -= buckets[i].size ();
+    buckets[i].clear ();
+  }
+}
+
+template<class KeyT, class ValueT>
+ValueT &HashMap<KeyT, ValueT>::operator[] (const KeyT &key)
+{
+  if (contains_key (key))
+  {
+    return at (key);
+  }
+  insert (key, ValueT ());
+  return at (key);
+}
+template<class KeyT, class ValueT>
+bool HashMap<KeyT, ValueT>::operator== (const HashMap<KeyT, ValueT> &other)
+{
+  if (size () != other.size ())
+  { return false; }
+  for (const auto &p: *this)
+  {
+    if (!other.contains_key (p.first))
+    {
+      return false;
+    }
+    if (other.at (p.first) != p.second)
+    {
+      return false;
+    }
+  }
+  return true;
+}
+template<class KeyT, class ValueT>
+bool HashMap<KeyT, ValueT>::contained_in (const HashMap<KeyT, ValueT>
+                                          &other) const
+{
+  for (const auto &p: *this)
+  {
+    if (!other.contains_key (p.first))
+    {
+      return false;
+    }
+    if (p.second != other.at (p.first))
+    {
+      return false;
+    }
+  }
+  return true;
+}
+template<class KeyT, class ValueT>
+bool HashMap<KeyT, ValueT>::operator!= (const HashMap<KeyT, ValueT> &other)
+{
+  return (!operator== (other));
+}
+
+template<class KeyT, class ValueT>
+const ValueT &HashMap<KeyT, ValueT>::at (const KeyT &key) const
+{
+  unsigned int hash_value = hash (key);
+  for (size_t i = 0; i < buckets[hash_value].size (); ++i)
+  {
+    if (buckets[hash_value].at (i).first == key)
+    {
+      return buckets[hash_value].at (i).second;
+    }
+  }
+  throw std::runtime_error (KEY_NOT_FOUND_MSG);
+}
+template<class KeyT, class ValueT>
+void HashMap<KeyT, ValueT>::adjust_capacity_to_balance_load_factor ()
+{
+  if (_size == 0)
+  {
+    _capacity = 1;
+    return;
+  }
+  if (get_load_factor () < lower_load_factor)
+  {
+    while (get_load_factor () < lower_load_factor && _capacity > MIN_CAPACITY)
+    {
+      _capacity /= 2;
+    }
+    return;
+  }
+  else
+  {
+    while (get_load_factor () > upper_load_factor)
+    {
+      _capacity *= 2;
+    }
+  }
+}
+template<class KeyT, class ValueT>
+bool HashMap<KeyT, ValueT>::load_factor_okay ()
+{
+  return (get_load_factor () <= upper_load_factor && get_load_factor () >=
+                                                     lower_load_factor);
+}
 
 #endif //_HASHMAP_HPP_
