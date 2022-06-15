262c5cbbccef883b5ee9b88167a3572386ec3e8f
Ran <ranhoudine@gmail.com>
2022-06-15 23:18:27 +0300

    8

--- Dictionary.hpp
+++ Dictionary.hpp
@@ -1,37 +1,37 @@
 #include "HashMap.hpp"
-
+#include <iterator>
 #ifndef _DICTIONARY_HPP_
 #define _DICTIONARY_HPP_
 using std::string;
-class InvalidKey: std::invalid_argument
+using string_iterator = std::iterator<std::forward_iterator_tag, pair<string,
+string>>
+class InvalidKey : std::invalid_argument
 {
  public:
-  explicit InvalidKey(const string& what): std::invalid_argument(what) {}
+  explicit InvalidKey (const string &what) : std::invalid_argument (what)
+  {}
 };
 class Dictionary : public HashMap<string, string>
 {
  public:
-  bool erase (const string &key);
-  void update (HashMap::ConstIterator& start, const
-  HashMap::ConstIterator& end );
+  bool erase (const string &key) override;
+  void update (string_iterator start, string_iterator end)
+  {
+    for (string_iterator it = start; it != end; ++it)
+    {
+      insert((*it).first, (*it).second);
+    }
+  }
 
 };
 bool Dictionary::erase (const string &key)
 {
-  if (!HashMap::erase(key))
+  if (!HashMap::erase (key))
   {
     return true;
   }
-  throw InvalidKey(KEY_NOT_FOUND_MSG);
+  throw InvalidKey (KEY_NOT_FOUND_MSG);
 }
-void
-Dictionary::update (HashMap::ConstIterator &start,
-                    const HashMap::ConstIterator &end)
-{
-  while (start != end && start.operator->() != nullptr)
-  {
-    insert (start->first, start->second);
-    ++start;
-  }
-}
+
+
 #endif //_DICTIONARY_HPP_
